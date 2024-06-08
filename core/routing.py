71ccbcbbb298c2eb9b16edf0852295f180ae3c1a
damyks <damianojeh2@gmail.com>
2024-06-08 09:09:45 +0100

    ready to push

---
+++ core/routing.py
@@ -0,0 +1,16 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter , URLRouter
+import chat.routing
+
+
+from django.core.asgi import get_asgi_application
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_application,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
