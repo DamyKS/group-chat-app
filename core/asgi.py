809766506f096626f88bcb2e690addd0cd553dbc
damyks <damianojeh2@gmail.com>
2024-06-15 14:05:38 +0100

    typo

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,9 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')typo
 import django 
 django.setup()
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 
 application = get_asgi_application()
