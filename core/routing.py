3ef1782b8f04e7fd5ff1213885e9b74ec11ae165
damyks <damianojeh2@gmail.com>
2024-06-20 06:52:42 +0100

    added os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings') to core/routing

--- core/routing.py
+++ core/routing.py
@@ -1,7 +1,8 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter , URLRouter
 import chat.routing
-
+import os 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 from django.core.asgi import get_asgi_application
 django_asgi_application = get_asgi_application()
