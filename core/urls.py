e6b6348a719b14e41dca441eb6493a8facab863e
damyks <damianojeh@gmail.com>
2024-12-28 17:50:14 +0100

    fixed homepage url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('chat/', include('chat.urls')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("chat.urls")),
 ]
